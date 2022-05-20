897ba7307e0c37379e2fad2ad2607fa605eb5c17
MH51 <38562406+MH51@users.noreply.github.com>
2022-05-20 17:59:51 +0530

    Create r.py

---
+++ r.py
@@ -0,0 +1,20 @@
+#rminder of 2 num
+print("hello")
+#sum of two number 
+a=int(input('enter number 1:'));
+b=int(input('enter number 2:'));
+
+print(c)
+if a>b:
+	print("A is big") ;
+else:
+	print("A is big") ;
+d=a%b
+print(d)
+#check datatype uning 1nput()
+#e=input("enter name :  ")
+print(a>b)
+print( type(a) )
+#find avg
+e=c/2
+print(e )
